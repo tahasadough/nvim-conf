ba36ef229f3a762b857692f788ac274e2693a36c
Taha Sadough <tsadough.eng@gmail.com>
2026-07-10 10:32:25 +0330

    add useful default keybinds Allows easy discovery and user overrides. snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -34,11 +34,89 @@ return {
   },
   keys = {
     {
-      "<leader><leader>",
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "Explorer",
+    },
+    {
+      "<leader><space>",
       function()
         Snacks.picker.smart()
       end,
       desc = "Smart Find Files",
+    },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>fr",
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = "Recent Files",
+    },
+    {
+      "<leader>fh",
+      function()
+        Snacks.picker.help()
+      end,
+      desc = "Help",
+    },
+    {
+      "<leader>fd",
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = "Diagnostics",
+    },
+
+    {
+      "<leader>z",
+      function()
+        Snacks.zen()
+      end,
+      desc = "Zen Mode",
+    },
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Scratch Buffer",
+    },
+    {
+      "<c-/>",
+      function()
+        Snacks.terminal()
+      end,
+      desc = "Terminal",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>n",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Notifications",
     },
   },
 }
